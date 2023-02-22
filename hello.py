4a74f36f45fee1da841062e9a38ef876ac17ca38
solssak <gswoong1012@gmail.com>
2023-02-22 11:53:02 +0900

    programmer : 230222

--- hello.py
+++ hello.py
@@ -352,3 +352,7 @@ def solution(array, n):
     answer = array.count(n)
     return answer
     
+# 특정 문자 제거하기
+def solution(my_string, letter):
+    answer = my_string.replace(letter,"")
+    return answer
