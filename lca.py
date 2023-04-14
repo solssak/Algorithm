263571299ec93817bf50ad97e65044edd011a3df
solssak <gswoong1012@gmail.com>
2023-04-14 16:48:54 +0900

    LCA 예제

--- lca.py
+++ lca.py
@@ -1,10 +1,29 @@
+# DFS 후위순위 (postorder)
+
+def LCA(root, q, p):
+    if root == None:
+        return None
+    
+    left = LCA(root.left, p, q)
+    right = LCA(root.right, p, q)
+
+    if root == q or root == p:
+        return root
+    elif left and right:
+        return root
+    return left or right
+
+
+
+
 def LCA(root, p, q):
     if root == None:
         return None
     
-    left = LCA(root, p, q)
-    right = LCA(root, p, q)
-    if root == p or root == q:
+    left = LCA(root.left, p, q)
+    right = LCA(root.right, p, q)
+
+    if root == q or root == p:
         return root
     elif left and right:
         return root
