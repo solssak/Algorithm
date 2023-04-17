5611d3237f029d982fc9dd7e902646d87757f6d3
solssak <gswoong1012@gmail.com>
2023-04-18 02:12:57 +0900

    dfs 예제 3번

---
+++ dfs3.py
@@ -0,0 +1,12 @@
+# postorder
+
+from collections import deque
+
+def maxDepth(root):
+    
+    if root is None:
+        return 0
+    
+    left_depth = maxDepth(root.left)
+    right_depth = maxDepth(root.right)
+    return max(left_depth, right_depth) +1
