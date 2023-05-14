a95d80d50f01752ef39163914c8c639c0bd5955d
solssak <gswoong1012@gmail.com>
2023-05-14 23:11:05 +0900

    programmers Lv.1 - 1문제

--- lv1.py
+++ lv1.py
@@ -527,3 +527,15 @@ def solution(name, yearning, photo):
                 count += yearning[idx]
         answer.append(count)
     return answer
+
+# 없는 숫자 더하기
+
+def solution(numbers):
+    answer = 0
+
+
+    for i in range(1, 10):
+        if i not in numbers:
+            answer += i
+
+    return answer
