fe530c766be707390ef052bb78c0efcd251c2728
solssak <gswoong1012@gmail.com>
2023-05-22 23:39:46 +0900

    programmers lv.2 - 1문제

--- lv2.py
+++ lv2.py
@@ -88,3 +88,27 @@ def solution(n):
                 break
     
     return answer
+
+# 기능 개발
+
+import math
+
+def solution(progresses, speeds):
+    n = len(progresses)
+    date_left = []
+    answer = []
+    
+    for i in range(n):
+        progress_left = 100 - progresses[i]
+        cut = math.ceil(progress_left / speeds[i])
+        date_left.append(cut)
+        
+    while date_left:
+        left = date_left.pop(0)
+        result = 1
+        while len(date_left) != 0 and left >= date_left[0]:
+            result += 1
+            date_left.pop(0)
+        answer.append(result)
+            
+    return answer
