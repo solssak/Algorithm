b899232d1a0aac5b97ed74a1484fdc2de047b366
solssak <gswoong1012@gmail.com>
2023-06-19 23:20:09 +0900

    programmers Lv.2 - 1문제

--- lv2.py
+++ lv2.py
@@ -113,4 +113,16 @@ def solution(progresses, speeds):
             
     return answer
 
-........
+# 멀리 뛰기
+
+def solution(n):
+    if n < 3:
+        return n
+    
+    d = [0] * (n+1)
+    d[1] = 1
+    d[2] = 2
+    for i in range(3, n + 1):
+        d[i] = d[i-1] + d[i-2]
+        
+    return d[n]%1234567
